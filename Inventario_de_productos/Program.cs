e836ecb467c04f3cda069ffd79bb55326ff6f49f
adria <adria@REX54X>
2026-03-17 08:55:11 -0600

    Mitadad del programa

--- Inventario_de_productos/Program.cs
+++ Inventario_de_productos/Program.cs
@@ -1,5 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 List<Producto> productos = new List<Producto>();
+Console.Write("Cuantos productos desea ingresar:");
+int cantidad = int.Parse(Console.ReadLine());
+for (int i = 0; i < cantidad; i++)
+{
+    Producto p = new Producto();
+    Console.Write($"\n{i+1}. Nombre del producto:");
+    p.Nombre = Console.ReadLine();
+    Console.Write("  Precio:Q");
+    p.Precio = double.Parse(Console.ReadLine());
+    Console.Write("  Cantidad:");
+    p.Cantidad = int.Parse(Console.ReadLine());
+    productos.Add(p);
+}
 class Producto
 {
     public string Nombre;
