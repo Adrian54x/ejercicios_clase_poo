0e5c4634e5050b8ad8e5a1ee6a18480bc069a797
adria <adria@REX54X>
2026-03-17 08:49:31 -0600

    Estructura

---
+++ Inventario_de_productos/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<Producto> productos = new List<Producto>();
+class Producto
+{
+    public string Nombre;
+    public double Precio;
+    public int Cantidad;
+    public double ValorTotal()
+    {
+        return Precio * Cantidad;
+    }
+    public string Stock()
+    {
+        if (Cantidad == 0)
+        {
+            return "Sin Existencia";
+        }
+        else if(Cantidad < 5)
+        {
+            return "Stock Bajo";
+        }
+        else
+        {
+            return "Stock Suficiente";
+        }
+    }
+}
