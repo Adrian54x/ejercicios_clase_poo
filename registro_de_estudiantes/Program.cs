ded0c99311e448f91d5b3024362234fb9be3e122
adria <adria@REX54X>
2026-03-17 08:27:22 -0600

    mitad del programa

--- registro_de_estudiantes/Program.cs
+++ registro_de_estudiantes/Program.cs
@@ -1,6 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 List<Estudiante> estudiantes = new List<Estudiante>();
+Console.Write("Cuantos estudiantes desea ingresar:");
+int cantidad = int.Parse(Console.ReadLine());
+for (int i = 0; i < cantidad; i++)
+{
+    Estudiante e = new Estudiante();
+    Console.Write($"{i + 1}. Nombre de estudiante:");
+    e.Nombre = Console.ReadLine();
+    Console.Write(" Nota1:");
+    e.Nota1 = double.Parse(Console.ReadLine());
+    Console.Write(" Nota2:");
+    e.Nota2 = double.Parse(Console.ReadLine());
+    Console.Write(" Nota3:");
+    e.Nota3 = double.Parse(Console.ReadLine());
+    estudiantes.Add(e);
+}
 class Estudiante
 {
     public string Nombre;
