f518b05684157a3fd2f2320f43ac4e0b197b2847
adria <adria@REX54X>
2026-03-17 08:21:13 -0600

    Estructura

---
+++ registro_de_estudiantes/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+List<Estudiante> estudiantes = new List<Estudiante>();
+class Estudiante
+{
+    public string Nombre;
+    public double Nota1;
+    public double Nota2;
+    public double Nota3;
+    public double PromedioNota()
+    {
+        return (Nota1 + Nota2 + Nota3)/3;
+    }
+    public string EstadoDeEstudiante()
+    {
+        if (PromedioNota() >= 60)
+        {
+            return "Aprovado";
+        }
+        else
+        {
+            return "Reprobado";
+        }
+    }
+    public void InformacionEstudiante()
+    {
+        Console.WriteLine($"\nNombre:{Nombre} \nPromedio:{PromedioNota()} \nEstado:{EstadoDeEstudiante}");
+    }
+}
